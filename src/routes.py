01fe54528bc5970e534b3f84416b220a888c4ce9
Raul <runnerdude08@gmail.com>
2018-01-06 00:22:16 -0800

    Created users table/model, connected to PostgresQL DB with SQLAlchemy

--- src/routes.py
+++ src/routes.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from .models import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learning-flask'
+db.init_app(app)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/about")
 def about():
