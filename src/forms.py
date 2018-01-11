a26da12409c20e84e9b3a38ae49d9f4e68b4547b
Raul <runnerdude08@gmail.com>
2018-01-10 19:43:29 -0800

    Finished authentication/authorization on login and signup, protected home and signup/login forms from being seen when already logged in. Implemented logout

--- src/forms.py
+++ src/forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(Form):
-    firstname = StringField('First name')
-    lastname = StringField('Last name')
-    email = StringField('Email')
-    password = PasswordField('Password')
+    firstname = StringField('First name', validators=[DataRequired('Please enter your first name')])
+    lastname = StringField('Last name', validators=[DataRequired('Please enter your last name')])
+    email = StringField('Email', validators=[DataRequired('Valid Email address required'), Email('Must be a valid email address')])
+    password = PasswordField('Password', validators=[DataRequired('Password is required'), Length(min=6, message='Password must be at least 6 characters long.')])
     submit = SubmitField('Sign up!')
+
+class LoginForm(Form):
+    email = StringField('Email Address', validators=[DataRequired('Valid Email address required'), Email('Email must be valid email address')])
+    password = PasswordField('Password', validators=[DataRequired('Password required')])
+    submit = SubmitField('Log in')
--- src/routes.py
+++ src/routes.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
-from models import db
-from forms import SignupForm
+from flask import Flask, render_template, request, session, redirect, url_for
+from models import db, User
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learning-flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/learningflask'
 app.secret_key = "development-key"
 
 db.init_app(app)
@@ -21,12 +21,61 @@ def about():
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
+    if 'email' in session:
+        return redirect(url_for('home'))
+
     form = SignupForm()
 
     if request.method == 'POST':
-        return 'Success!'
+        if not form.validate():
+            return render_template("signup.html", form=form)
+        else:
+            newuser = User(form.firstname.data, form.lastname.data, form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+
+            session['email'] = newuser.email
+            return redirect(url_for('home'))
     else:
         return render_template("signup.html", form=form)
+
+
+@app.route("/logout")
+def signout():
+    session.pop('email', None)
+    return redirect(url_for('index'))
+
+
+@app.route("/home")
+def home():
+    if 'email' not in session:
+        return redirect(url_for('login'))
+    return render_template("home.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if 'email' in session:
+        return redirect(url_for('home'))
+    
+    login_form = LoginForm()
+
+    if request.method == 'POST':
+        if not login_form.validate():
+            return render_template("login.html", form=login_form)
+
+        email = login_form.email.data
+        password = login_form.password.data
+
+        user = User.query.filter_by(email=email).first()
+        if user is not None and user.check_password(password):
+            session['email'] = login_form.email.data
+            return redirect(url_for('home'))
+        else:
+            return redirect(url_for('login'))
+    else:
+        return render_template("login.html", form=login_form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
